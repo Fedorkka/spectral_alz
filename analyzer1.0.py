cc06274859cc7aaee71613ef115e8143350aaea2
Fedor Uzhegov <fuzhegv@mail.ru>
2019-02-27 23:18:18 +0300

    v0.2

--- analyzer1.0.py
+++ analyzer1.0.py
@@ -16,6 +16,9 @@ b_x = 0
 b_y = 0
 d1 = []
 d2 = []
+cr=[]
+cb=[]
+cg=[]
 gr = []
 r = []
 g = []
@@ -40,18 +43,16 @@ def main_menu():
     root.mainloop()
 
 
-def move_line():
-    global d1, d2
+def move_line(line):
+    global d1, d2, cr, cb, cg
     x_m, y_m = pygame.mouse.get_pos()
-    d1[1] = y_m
-    d2[1] = y_m
+    if line== "black":
+        d1[1] = y_m
+        d2[1] = y_m
 
 
-def open_img():
-    global d1, d2, root, filename
-    root.withdraw()
-    filename = askopenfilename()
-    img = Image.open(filename)
+def crop_img(img):
+    global d1, d2, max_d
     if img.size[0] > img.size[1]:
         new_width = 780
         new_height = int(780 * img.size[1] / img.size[0])
@@ -68,6 +69,15 @@ def open_img():
         d1 = [b_x, 400]
         d2 = [b_x + new_width, 400]
         max_d = [b_y, b_y + new_height]
+    return new_width, new_height, b_x, b_y
+
+
+def open_img():
+    global d1, d2, root, filename, max_d
+    root.withdraw()
+    filename = askopenfilename()
+    img = Image.open(filename)
+    new_width, new_height, b_x, b_y = crop_img(img)
     img = img.resize((new_width, new_height), Image.ANTIALIAS)
     mode = img.mode
     size = img.size
@@ -85,8 +95,9 @@ def open_img():
                 if pygame.mouse.get_pressed()[0] == 1:
                     if ((d1[1] - 5) < y_m < (d1[1] + 5)) and (d1[0] < x_m < d2[0]):
                         line_d = 1
+                        color= "black"
                 if pygame.mouse.get_pressed()[1] == 1:
-                    draw_grath()
+                    draw_graph(color)
         screen.fill(white)
         x_m, y_m = pygame.mouse.get_pos()
         if pygame.mouse.get_pressed()[0] == 0:
@@ -101,14 +112,60 @@ def open_img():
             d1[1] = max_d[0] + 5
             d2[1] = max_d[0] + 5
         pygame.draw.line(screen, black, [d1[0], d1[1]], [d2[0], d2[1]], 2)
-        pygame
         pygame.display.flip()
         clock.tick(60)
     root.deiconify()
     pygame.quit()
 
 
-def draw_grath():
+def calibrate():
+
+    global d1, d2, root, filename, max_d
+    root.withdraw()
+    filename = askopenfilename()
+    img = Image.open(filename)
+    new_width, new_height, b_x, b_y = crop_img(img)
+    img = img.resize((new_width, new_height), Image.ANTIALIAS)
+    mode = img.mode
+    size = img.size
+    data = img.tobytes()
+    img_p = pygame.image.fromstring(data, size, mode)
+    screen = pygame.display.set_mode((w, h))
+    screen.fill(white)
+    done = False
+    clock = pygame.time.Clock()
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+                if pygame.mouse.get_pressed()[0] == 1:
+                    if ((cr[1] - 5) < x_m < (cr[1] + 5)) and (cr[0] < y_m < cr[0]):
+                        line_d = 1
+                        color = "red"
+                if pygame.mouse.get_pressed()[1] == 1:
+                    draw_graph()
+        screen.fill(white)
+        x_m, y_m = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0] == 0:
+            line_d = 0
+        if line_d == 1 and max_d[0] < cr[1] < max_d[1]:
+            move_line(color)
+        screen.blit(img_p, (b_x, b_y))
+        if d1[1] > max_d[1] - 5:
+            d1[1] = max_d[1] - 5
+            d2[1] = max_d[1] - 5
+        elif d1[1] < max_d[0] + 5:
+            d1[1] = max_d[0] + 5
+            d2[1] = max_d[0] + 5
+        pygame.draw.line(screen, black, [d1[0], d1[1]], [d2[0], d2[1]], 2)
+        pygame.display.flip()
+        clock.tick(60)
+    root.deiconify()
+    pygame.quit()
+
+
+def draw_graph():
+
 
     im = scipy.misc.imread(filename, flatten=False, mode='RGB')
     img = Image.open(filename)
